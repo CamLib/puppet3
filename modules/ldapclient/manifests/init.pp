0a4d7497ca89689c3d8a6ab20471f1f56f9c09d2
Tony Stevenson <tony@pc-tony.com>
2014-03-25 19:35:57 +0000

    Indent madness.

--- modules/ldapclient/manifests/init.pp
+++ modules/ldapclient/manifests/init.pp
@@ -4,16 +4,16 @@ class ldapclient {
 
   ## Define packages per OS.
   case $operatingsystem: { 
-   'FreeBSD': {
-     ## Now check for the version of the OS.
-     case $operatingsystemrelease: {
-       '10.0-RELEASE': {
-         $packages  =
-         [ 'openldap-client-24.38', 'nss_ldap-1.265_9',
-           'pam_ldap-1.8.6_2', 'pam_mkhomedir-0.2'
-         ],
-       }
-     }
+    'FreeBSD': {
+      ## Now check for the version of the OS.
+      case $operatingsystemrelease: {
+        '10.0-RELEASE': {
+          $packages  =
+          [ 'openldap-client-24.38', 'nss_ldap-1.265_9',
+            'pam_ldap-1.8.6_2', 'pam_mkhomedir-0.2'
+          ],
+        }
+      }
    }
    default =>
      [
