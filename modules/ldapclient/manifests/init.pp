8e3e2e272182aa266b7b43892d793d41c1c1a8ea
Tony Stevenson <tony@pc-tony.com>
2014-03-25 19:32:58 +0000

    Add initial working on the ldapclient module. Will be tested more as we add the files to deploy along with the package installs.

---
+++ modules/ldapclient/manifests/init.pp
@@ -0,0 +1,26 @@
+#/etc/puppet/modules/ldapclient/manifests/init.pp
+
+class ldapclient {
+
+  ## Define packages per OS.
+  case $operatingsystem: { 
+   'FreeBSD': {
+     ## Now check for the version of the OS.
+     case $operatingsystemrelease: {
+       '10.0-RELEASE': {
+         $packages  =
+         [ 'openldap-client-24.38', 'nss_ldap-1.265_9',
+           'pam_ldap-1.8.6_2', 'pam_mkhomedir-0.2'
+         ],
+       }
+     }
+   }
+   default =>
+     [
+     ]
+  }
+
+  package { $packages: 
+    ensure =>  installed,
+  }
+}
