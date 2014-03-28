d5809f038c5493b1fff252bb1a48ace562af3f89
Peter Heiner <p.heiner@gmail.com>
2014-03-28 16:23:45 +0000

    hiera'ify

--- modules/ldapclient/manifests/init.pp
+++ modules/ldapclient/manifests/init.pp
@@ -1,24 +1,8 @@
 #/etc/puppet/modules/ldapclient/manifests/init.pp
 
-class ldapclient {
-
-  ## Define packages per OS.
-  case $operatingsystem: { 
-    'FreeBSD': {
-      ## Now check for the version of the OS.
-      case $operatingsystemrelease: {
-        '10.0-RELEASE': {
-          $packages  =
-          [ 'openldap-client-24.38', 'nss_ldap-1.265_9',
-            'pam_ldap-1.8.6_2', 'pam_mkhomedir-0.2'
-          ],
-        }
-      }
-   }
-   default =>
-     [
-     ]
-  }
+class ldapclient (
+  $packages  = [] ,
+) {
 
   package { $packages: 
     ensure =>  installed,
