858d1847440bc9c7ef2b3d21b4d4c529625b2e5b
Tony Stevenson <tony@pc-tony.com>
2014-04-07 17:53:58 +0100

    Testing scoping

--- modules/ldapclient/manifests/init.pp
+++ modules/ldapclient/manifests/init.pp
@@ -1,11 +1,12 @@
 #/etc/puppet/modules/ldapclient/manifests/init.pp
 
-
 class ldapclient (
   $packages  = [],
 ) {
 
+$pkgprovider = '',
+
   package { $packages: 
-    ensure =>  installed,
+    ensure   =>  installed,
   }
 }
