e587220134a4cf81c9bfe1dc27973250a1ddf895
Tony Stevenson <tony@pc-tony.com>
2014-04-08 14:04:29 +0100

    Add a new template for /usr/local/etc/ldap.conf and /usr/local/etc/nss_ldap.conf Deploy file, and symlink one to the other.

--- modules/ldapclient/manifests/init.pp
+++ modules/ldapclient/manifests/init.pp
@@ -10,7 +10,14 @@ class ldapclient (
     ensure   =>  installed,
   }
 
-  file { '/usr/local/etc/openldap/ldap.conf':
-    content => template('/usr/local/etc/puppet/modules/ldapclient/templates/usr/local/etc/openldap/ldap.conf.erb'),
+  file { 
+    '/usr/local/etc/openldap/ldap.conf':
+      content => template('/usr/local/etc/puppet/modules/ldapclient/templates/usr/local/etc/openldap/ldap.conf.erb');
+    '/usr/local/etc/ldap.conf':
+      content => template('/usr/local/etc/puppet/modules/ldapclient/templates/usr/local/etc/ldap.conf.erb');
+    '/usr/local/etc/nss_ldap.conf':
+      ensure  => link,
+      target  => '/usr/local/etc/ldap.conf',
+      require => File['/usr/local/etc/ldap.conf'];
   }
 }
