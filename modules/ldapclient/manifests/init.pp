b37718090e4dae6710f925e52956ea8f7aec4d7c
Tony Stevenson <tony@pc-tony.com>
2014-04-09 22:47:52 +0100

    Bump all templates into a per project templates that is not recursive

--- modules/ldapclient/manifests/init.pp
+++ modules/ldapclient/manifests/init.pp
@@ -11,9 +11,9 @@ class ldapclient (
 
   file { 
     '/usr/local/etc/openldap/ldap.conf':
-      content => template('/usr/local/etc/puppet/modules/ldapclient/templates/usr/local/etc/openldap/ldap.conf.erb');
+      content => template('/usr/local/etc/puppet/modules/ldapclient/templates/openldap_ldap.conf.erb');
     '/usr/local/etc/ldap.conf':
-      content => template('/usr/local/etc/puppet/modules/ldapclient/templates/usr/local/etc/ldap.conf.erb');
+      content => template('/usr/local/etc/puppet/modules/ldapclient/templates/ldap.conf.erb');
     '/usr/local/etc/nss_ldap.conf':
       ensure  => link,
       target  => '/usr/local/etc/ldap.conf',
@@ -22,13 +22,13 @@ class ldapclient (
       source  => 'puppet:///modules/ldapclient/etc/nsswitch.conf',
       require => File['/usr/local/etc/ldap.conf'];
     '/etc/pam.d/system':
-      content => template('/usr/local/etc/puppet/modules/ldapclient/templates/etc/pam.d/system.erb'),
+      content => template('/usr/local/etc/puppet/modules/ldapclient/templates/pam_system.erb'),
       require => File['/usr/local/etc/ldap.conf'];
     '/etc/pam.d/sshd':
-      content => template('/usr/local/etc/puppet/modules/ldapclient/templates/etc/pam.d/sshd.erb'),
+      content => template('/usr/local/etc/puppet/modules/ldapclient/templates/pam_sshd.erb'),
       require => File['/usr/local/etc/ldap.conf'];
     '/etc/pam.d/su':
-      content => template('/usr/local/etc/puppet/modules/ldapclient/templates/etc/pam.d/su.erb'),
+      content => template('/usr/local/etc/puppet/modules/ldapclient/templates/pam_su.erb'),
       require => File['/usr/local/etc/ldap.conf'];
   }
 }
