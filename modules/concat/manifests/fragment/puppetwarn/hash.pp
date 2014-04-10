d7a044616ca15f857bc3d37c7037cd71d43be0e2
Tony Stevenson <tony@pc-tony.com>
2014-04-10 20:16:20 +0100

    Multiline variables are do-able. I should have remembered that.

--- modules/concat/manifests/fragment/puppetwarn/hash.pp
+++ modules/concat/manifests/fragment/puppetwarn/hash.pp
@@ -1,7 +1,16 @@
 #
 define concat::fragment::puppetwarn::hash(
     $target,
-    $content="###\n##         ..:: WARNING ::..\n##\n## This file is managed by puppet.\n## All local changes will be lost at the next puppet run.\n##\n###\n\n",
+    $content="
+###
+##           ..:: WARNING ::..
+##
+##    This file is managed by puppet.
+## All local changes will be lost during 
+##         the next puppet run.
+##
+###
+",
     $source=undef,
     $order=10,
     $ensure = 'present',
