75cfbd00a2e97c0160c9eb7a9d35f9a75799ef8c
Tony Stevenson <tony@pc-tony.com>
2014-04-14 18:06:36 +0100

    Tidy up and standardise

--- modules/pam/manifests/init.pp
+++ modules/pam/manifests/init.pp
@@ -5,86 +5,85 @@ class pam (
   ## Files being managed. These are the default
   ## values. As these seem like generic sane defaults.
   ## However you should note that hiera should be populating them.
-  $pam_zzz    = "/etc/pam.d/zzz",
-  $pam_su     = "/etc/pam.d/su",
-  $pam_system = "/etc/pam.d/system",
-  $pam_sshd   = "/etc/pam.d/sshd",
+  $pam_su     = "",
+  $pam_system = "",
+  $pam_sshd   = "",
  
 
   ## Content variables, as found in hiera data
-  $generic_header  ='',
+  $generic_header  = "",
 
 
   ## Data variables
-  $sshd_10_facility   = '',
-  $sshd_10_control    = '',
-  $sshd_10_modulepath = '',
-  $sshd_10_modopts    = '',
+  $sshd_10_facility   = "",
+  $sshd_10_control    = "",
+  $sshd_10_modulepath = "",
+  $sshd_10_modopts    = "",
 
   $sshd_15_facility   = "",
   $sshd_15_control    = "",
   $sshd_15_modulepath = "",
-  $sshd_15_modopts = "",
+  $sshd_15_modopts    = "",
 
   $sshd_20_facility   = "",
   $sshd_20_control    = "",
   $sshd_20_modulepath = "",
-  $sshd_20_modopts = "",
+  $sshd_20_modopts    = "",
 
   $sshd_25_facility   = "",
   $sshd_25_control    = "",
   $sshd_25_modulepath = "",
-  $sshd_25_modopts = "",
+  $sshd_25_modopts    = "",
 
   $sshd_30_facility   = "",
   $sshd_30_control    = "",
   $sshd_30_modulepath = "",
-  $sshd_30_modopts = "",
+  $sshd_30_modopts    = "",
 
   $sshd_35_facility   = "",
   $sshd_35_control    = "",
   $sshd_35_modulepath = "",
-  $sshd_35_modopts = "",
+  $sshd_35_modopts    = "",
 
   $sshd_50_facility   = "",
   $sshd_50_control    = "",
   $sshd_50_modulepath = "",
-  $sshd_50_modopts = "",
+  $sshd_50_modopts    = "",
 
   $sshd_55_facility   = "",
   $sshd_55_control    = "",
   $sshd_55_modulepath = "",
-  $sshd_55_modopts = "",
+  $sshd_55_modopts    = "",
 
   $sshd_60_facility   = "",
   $sshd_60_control    = "",
   $sshd_60_modulepath = "",
-  $sshd_60_modopts = "",
+  $sshd_60_modopts    = "",
 
   $sshd_65_facility   = "",
   $sshd_65_control    = "",
   $sshd_65_modulepath = "",
-  $sshd_65_modopts = "",
+  $sshd_65_modopts    = "",
 
   $sshd_70_facility   = "",
   $sshd_70_control    = "",
   $sshd_70_modulepath = "",
-  $sshd_70_modopts = "",
+  $sshd_70_modopts    = "",
 
   $sshd_80_facility   = "",
   $sshd_80_control    = "",
   $sshd_80_modulepath = "",
-  $sshd_80_modopts = "",
+  $sshd_80_modopts    = "",
 
   $sshd_85_facility   = "",
   $sshd_85_control    = "",
   $sshd_85_modulepath = "",
-  $sshd_85_modopts = "",
+  $sshd_85_modopts    = "",
 
   $sshd_90_facility   = "",
   $sshd_90_control    = "",
   $sshd_90_modulepath = "",
-  $sshd_90_modopts = "",
+  $sshd_90_modopts    = "",
 
   $sshd_95_facility   = "",
   $sshd_95_control    = "",
@@ -94,16 +93,10 @@ class pam (
   $sshd_100_facility   = "",
   $sshd_100_control    = "",
   $sshd_100_modulepath = "",
-  $sshd_100_modopts = "",
+  $sshd_100_modopts    = "",
 ) {
 
   # Declare the files to be created using concat
-  concat { $pam_zzz:
-    group  => wheel,
-    owner  => root,
-    mode   => 0644,
-  }
-
   concat { $pam_sshd:
     group  => wheel,
     owner  => root,
@@ -123,11 +116,6 @@ class pam (
   #}
 
 
-  ## Enforce the addition of our puppet warning header
-  concat::fragment::puppetwarn::hash{"pam-zzz-puppetwarn":
-    target  => $pam_zzz,
-  }
-
   concat::fragment::puppetwarn::hash{"pam-sshd-puppetwarn":
     target  => $pam_sshd,
   }
@@ -140,12 +128,6 @@ class pam (
     #target  => $pam_system,
   #}
 
-  ## Drop in the default header for the file
-  concat::fragment{"pam-zzz-header":
-    target  => $pam_zzz,
-    content => $generic_header,
-    order   => 05,
-  }
 
   concat::fragment{"pam-sshd-header":
     target  => $pam_sshd,
@@ -165,15 +147,6 @@ class pam (
     #order   => 05,
   #}
 
-
-  pam::insertline{"pam-zzz-test":
-    target         => $pam_zzz,
-    order          => "55",
-    pam_facility   => "auth",
-    pam_control    => "required",
-    pam_modulepath => "pam_opie.so",
-    pam_modopts    => "no_warn",
-  }
 
 
   pam::insertline{"pam-sshd-10":
