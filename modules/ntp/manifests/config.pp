f50a3e6a7d9788640ea610c45f245bfde5fdbcf0
Peter Heiner <aavkvard@gmail.com>
2014-04-25 17:57:26 +0100

    don't set file modes

--- modules/ntp/manifests/config.pp
+++ modules/ntp/manifests/config.pp
@@ -4,12 +4,6 @@ class ntp::config (
   $statsdir  = $ntp::statsdir,
 
 ) {
-
-  File{
-    owner   => root,
-    group   => root,
-    mode    => 644,
-  }
  
   file{'/etc/ntp.conf':
     content => template('ntp/ntp.conf.erb');
